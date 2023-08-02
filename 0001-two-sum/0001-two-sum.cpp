ca6f57e239faf6032f046179a001c9ed49ea6065
Jay Li <52246579+jaynraven@users.noreply.github.com>
2023-08-02 23:48:34 +0800

    Time: 310 ms (37.15%), Space: 10.3 MB (59.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-                for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++)
         {
             for(int j = i + 1; j < nums.size(); j++)
             {
