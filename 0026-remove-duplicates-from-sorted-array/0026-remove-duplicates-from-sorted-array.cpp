84d2b25da3fa88e9f6115620fe8fca600ad349f6
Jay Li <52246579+jaynraven@users.noreply.github.com>
2023-08-03 18:02:39 +0800

    Time: 7 ms (92.87%), Space: 18.3 MB (88.03%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left=0;
+        for(int right=0;right<nums.size();right++){
+            if(nums[left]!=nums[right]){
+                nums[++left]=nums[right--];
+            }
+        }
+        return left+1;
+    }
+};
